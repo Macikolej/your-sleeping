104355d0f33707d2f2116bf063500f457ab308ae
Macikolej <oiejnik152@gmail.com>
2023-01-29 21:00:04 +0100

    chaning the router

--- src/features/Router/index.jsx
+++ src/features/Router/index.jsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import { StartPage } from "features/StartPage";
 import { AdminPage } from "features/AdminPage";
 
 export const Router = () => {
 	return (
-		<BrowserRouter>
+		<HashRouter>
 			<Routes>
 				<Route path="/" element={<StartPage />} />
 				<Route path="/admin" element={<AdminPage />} />
 				<Route path="/host" element={<StartPage />} />
 				<Route path="/client" element={<StartPage />} />
 			</Routes>
-		</BrowserRouter>
+		</HashRouter>
 	);
 };
